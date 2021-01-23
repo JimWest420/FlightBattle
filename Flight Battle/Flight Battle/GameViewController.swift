709f96a7d5aa168a6387a7c44c509d2701daabcb
JimWest420 <jim_west_93@icloud.com>
2021-01-23 03:09:42 +0300

    Move position

--- Flight Battle/Flight Battle/GameViewController.swift
+++ Flight Battle/Flight Battle/GameViewController.swift
@@ -38,7 +38,8 @@ class GameViewController: UIViewController {
         scene.rootNode.addChildNode(ambientLightNode)
         
         // retrieve the ship node
-        //let ship = scene.rootNode.childNode(withName: "ship", recursively: true)!
+        let ship = scene.rootNode.childNode(withName: "ship", recursively: true)!
+        ship.position.z = -105 // задаем координату для самолета
         
         // animate the 3d object
         //ship.runAction(SCNAction.repeatForever(SCNAction.rotateBy(x: 0, y: 2, z: 0, duration: 1)))
